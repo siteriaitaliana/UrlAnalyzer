ebdba161df9c5396d0f7322d3f70c01032faae5e
Lorenzo Urbini <superluglor@gmail.com>
2011-06-17 17:31:35 +0100

    completely  broken soup addition

--- src/testtool/__init__.py
+++ src/testtool/__init__.py
@@ -17,8 +17,52 @@ if __name__ == "__main__":
     f.close()
     
     f = open('myfile.html','r')
-    print f.read()
-    f.close()
+    g = open('myurl','w')
+    
+
+    
+    #search all url
+def get_hyperlinks(url, source): 
+    
+    if url.endswith("/"):
+        url = url[:-1]
+    
+     urlPat = re.compile(r'<a [^<>]*?href=("|\')([^<>"\']*?)("|\')')
+    
+     result = re.findall(urlPat, source)
+    
+     urlList = []
+    
+     for item in result:
+         link = item[1]     
+         if link.startswith("http://") and link.startswith(url):
+             if link not in urlList:
+                 urlList.append(link)
+         elif link.startswith("/"):
+             link = url + link
+             if link not in urlList:
+                 urlList.append(link)
+         else:
+             link = url + "/" + link
+             if link not in urlList:
+                 urlList.append(link)
+     
+     return urlList
+    
+    print "Enter the URL: "
+    url = raw_input("> ")
+    usock = urllib2.urlopen(url)
+    data = usock.read()
+    usock.close()
+    print get_hyperlinks(url, data)
+    
+    #convert them from relative to absolute addresses
+    
+    #exclude the external link
+    
+    #print for each of them the link and the response
+      
+   
     
  
 
