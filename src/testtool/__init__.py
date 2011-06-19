4d6486192c9be519b04f179c2bba4b3b529443b5
Lorenzo Urbini <lorenzo.urbini@edgeint.net>
2011-06-19 10:46:07 +0100

    added print http status code

--- src/testtool/__init__.py
+++ src/testtool/__init__.py
@@ -10,25 +10,25 @@ from urlparse import urlparse
     data = usock.read()
     usock.close() 
 """
-#improvemtn, add 404 code chck of the main url, unit test, url arsing improve for params etc...
+#improvemts suggestions, add 404 code check of the main url, unit test, url arsing improve for params etc...
 
 #main method
 def __init__():
     
-    url = "http://network.civilservicelive.com"
+    url = "http://www.google.com"
     url_temp = urlparse(url)
     mainnetloc = url_temp.netloc
     urlList = []
+    codeList = {}
     
     getPage(url)
     get_hyperlinks(url, mainnetloc, urlList)
     getLocalUrl(urlList, mainnetloc)
+    getHttpStatusCode(urlList, codeList)
 
-    for address in urlList:
-        print address
+    for address,code in codeList.iteritems():
+            print address + "\t" + code
 
-
-#save web-page in a file
 def getPage(url):
     
     if url.endswith("/"):
@@ -41,7 +41,6 @@ def getPage(url):
     f.write(response.read())
     f.close()
  
-#search all urls_href
 def get_hyperlinks(url, mainnetloc, urlList): 
     
     f = file("myfile.html", "r")
@@ -76,7 +75,7 @@ def get_hyperlinks(url, mainnetloc, urlList):
             else:
                 finalmatch =  match.geturl()
   
-            if finalmatch not in urlList:
+            if finalmatch not in urlList[:]:
                 urlList.append(str(finalmatch))
                 
 
@@ -85,14 +84,17 @@ def getLocalUrl(urlList, mainnetloc):
         tmpaddress = urlparse(address)
         if tmpaddress.netloc != mainnetloc:
             urlList.remove(address)
-       
-      
- 
-    #convert them from relative to absolute addresses
-    
-    #exclude the external match
-    
-    #print for each of them the match and the response 
+
+def getHttpStatusCode(urlList, codeList):
+    for address in urlList[:]:
+        req = urllib2.Request(address)       
+        
+        try:
+            response = urllib2.urlopen(req)
+        except urllib2.HTTPError, e:
+            codeList[address]=str(e.code)             
+        else:
+            codeList[address]=str(response.code)
 
 
 if __name__ == "__main__":
